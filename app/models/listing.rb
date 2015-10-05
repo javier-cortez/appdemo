c6dbc2c7c54d1cf92e0d835e680c7a98efcb3c24
Javier Cortez <apps@devstarlab.com>
2015-10-05 00:10:49 -0600

    Add Figaro Gem

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,13 +1,15 @@
 require 'paperclip/media_type_spoof_detector'
 module Paperclip 
-class MediaTypeSpoofDetector
-def spoofed? 
-false 
-end 
-end 
+	class MediaTypeSpoofDetector
+		def spoofed? 
+			false 
+		end 
+	end 
 end
 
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
+	:storage => :dropbox,
+	:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
