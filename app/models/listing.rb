5ad574d906678bdfcf0d4bdeb434cad382b0798f
Javier Cortez <apps@devstarlab.com>
2015-10-05 01:06:18 -0600

    Finish setup for Dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -8,11 +8,14 @@ module Paperclip
 end
 
 class Listing < ActiveRecord::Base
-	if Rails.env.development?
+  if Rails.env.development?
     has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png"
   else
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
-	:storage => :dropbox,
-	:dropbox_credentials => Rails.root.join("config/dropbox.yml")
-	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
+        :storage => :dropbox,
+        :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+        :path => ":style/:id_:filename"
+  end
+
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
